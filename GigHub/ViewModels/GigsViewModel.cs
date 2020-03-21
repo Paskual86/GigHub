b48317b7c781d1992f777e42ff61b2d1018fffb7
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2020-03-21 09:38:24 -0300

    Refactoring Gigs

---
+++ GigHub/ViewModels/GigsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using GigHub.Models;
+using System.Collections.Generic;
+
+namespace GigHub.ViewModels
+{
+    public class GigsViewModel
+    {
+        public IEnumerable<Gig> UpcommingGigs { get; set; }
+        public bool ShowActions { get; set; }
+
+        public string Heading { get; set; }
+    }
+}
